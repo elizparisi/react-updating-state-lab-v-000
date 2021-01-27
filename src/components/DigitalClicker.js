093d37065f969928925709ec5715982c9077b400
Liz P <parisielizabeth@gmail.com>
2021-01-27 01:20:13 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,26 @@
 // Code DigitalClicker Component Here
+
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    };
+  }
+  
+  handleClick = () => {
+    let newCount = this.state.count + 1 
+    this.setState({count: newCount
+    })
+  }
+  
+  render() {
+    return(
+      <button onClick={this.handleClick}>Click</button>
+      )
+  }
+  
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,21 @@
 // Code YouTubeDebugger Component Here
+
+import React, { Component } from 'react';
+
+export default class YouTubeDebugger extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+      bitrate: 8,
+      video: {
+      resolution: '1080p'
+      }
+    }
+  }
+  
+  
+}
