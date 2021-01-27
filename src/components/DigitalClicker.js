3bbe9e06401af060aee04855201227c804a92cfc
Liz P <parisielizabeth@gmail.com>
2021-01-27 01:44:10 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -21,7 +21,7 @@ export default class DigitalClicker extends Component {
   
   render() {
     return(
-      <button onClick={this.timesClicked}></button>
+      <button onClick={this.handleClick}>{this.timesClicked}</button>
       )
   }
   
